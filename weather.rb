aac165f9a073e41d121e7ce4e2c1ce7b1723f9a1
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-12 06:45:15 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -6,6 +6,7 @@ $weather = {
 "temperature_farenheit" => 66.3}
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
+
 def location(weather)
 	return weather[:city] 
 end
